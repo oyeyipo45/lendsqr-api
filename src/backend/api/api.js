f8029ea4ee88c5c4d1f424f63a564a67aa1d0b67
damilola <oyeyipo45@gmail.com>
2022-03-22 07:59:03 +0100

    sort transactions by time created in decending order

--- src/backend/api/api.js
+++ src/backend/api/api.js
@@ -23,7 +23,7 @@ router.post('/register', validate, async (req, res, next) => {
     const availableUser = await knex('users').select().where({ email: email }).orWhere({username : username});
 
     if (availableUser.length > 0) {
-      return res.json({ status_code: 400, success: false, message: 'A user with this email or username already exist' });
+      return res.status(400).json({ status_code: 400, success: false, message: 'A user with this email or username already exist' });
     }
 
     // Hashing user's password
@@ -48,7 +48,7 @@ router.post('/register', validate, async (req, res, next) => {
       //Generating a JWT token
       const token = jwtGen(foundUser[0].username);
 
-      return res.json({ data:{ token : token}, status_code: 200, success: true, message: 'User created succesfully' });
+      return res.status(200).json({ data: { token: token }, status_code: 200, success: true, message: 'User created succesfully' });
     }
   } catch (error) {
     next(error);
@@ -71,7 +71,7 @@ router.post('/login', validate, async (req, res, next) => {
     }
     const token = jwtGen(user[0].username);
 
-    return res.json({ data: { ...user[0], token }, status_code: 200, success: true });
+    return res.status(200).json({ data: { ...user[0], token }, status_code: 200, success: true });
   } catch (error) {
     next(error);
   }
@@ -82,13 +82,13 @@ router.get('/user', auth, async (req, res, next) => {
   try {
     const user = await knex('users').select().where({ username: req.username });
     const wallet = await knex('wallet').select().where({ wallet_id: req.username });
-    const transactions = await knex('transactions').select().where({ wallet_id: req.username });
+    const transactions = await knex('transactions').select().where({ wallet_id: req.username }).orderBy('created_date', 'desc');
 
     // const token = jwtGen(5000);
 
     // console.log(token)
 
-    return res.json({ data: { user, wallet, transactions }, status_code: 200, success: true });
+    return res.status(200).json({ data: { user, wallet, transactions }, status_code: 200, success: true });
   } catch (error) {
     next(error);
   }
@@ -105,13 +105,16 @@ router.put('/fund-wallet', auth,  async (req, res, next) => {
 
     const verifiedAmount = payload.user
 
+    if (verifiedAmount >= 1000000) {
+        return res.status(400).json({ data: {}, status_code: 400, success: false, message: 'Max deposit is 1,000,000 per day' });
+    } 
      const wallet = await knex('wallet').select().where({ wallet_id: username });
 
       if (wallet.length === 0) {
-        return res.json({ data: {}, status_code: 400, success: false, message: 'user does not exist' });
+        return res.status(400).json({ data: {}, status_code: 400, success: false, message: 'user does not exist' });
       } else {
 
-        const credit = wallet[0].balance + +verifiedAmount;
+      const credit = wallet[0].balance + parseInt(verifiedAmount);
 
         const updated_wallet = await knex('wallet').where({ wallet_id: username }).update({ balance: credit });
 
@@ -127,7 +130,7 @@ router.put('/fund-wallet', auth,  async (req, res, next) => {
 
         const new_balance = await knex('wallet').select().where({ wallet_id: username });
 
-       return  res.json({ data: { ...new_balance[0] }, status_code: 200, success: true, message: 'Wallet funded sucessfully' });
+       return res.status(200).json({ data: { ...new_balance[0] }, status_code: 200, success: true, message: 'Wallet funded sucessfully' });
      }
     
   } catch (error) {
@@ -148,16 +151,20 @@ router.put('/withdraw', auth, async (req, res, next) => {
 
     const verifiedAmount = payload.user;
 
+    if (verifiedAmount >= 1000000) {
+      return res.status(400).json({ data: {}, status_code: 400, success: false, message: 'Max withdrawal is 1,000,000 per day' });
+    } 
+
     const wallet = await knex('wallet').select().where({ wallet_id: username });
 
     if (wallet.length === 0) {
-     return  res.json({ data: {}, status_code: 400, success: false, message: 'user does not exist' });
+     return res.status(400).json({ data: {}, status_code: 400, success: false, message: 'user does not exist' });
     } else {
       // Check wallet for sufficient balance
       const wallet_balance_validation = await knex('wallet').where({ wallet_id: username }).having('balance', '<', verifiedAmount);
 
       if (wallet_balance_validation.length > 0) {
-       return  res.json({ data: {}, status_code: 400, success: false, message: 'Insuffucient funds' });
+       return res.status(400).json({ data: {}, status_code: 400, success: false, message: 'Insuffucient funds' });
       }
 
       const debit = wallet[0].balance - verifiedAmount;
@@ -176,7 +183,7 @@ router.put('/withdraw', auth, async (req, res, next) => {
 
       const sender_transaction = await knex('transactions').insert({ ...transaction_data });
 
-      return res.json({ data: { ...new_balance[0] }, status_code: 200, success: true, message: 'Withdrawal sucessfully' });
+      return res.status(200).json({ data: { ...new_balance[0] }, status_code: 200, success: true, message: 'Withdrawal sucessful' });
     }
   } catch (error) {
     console.log(error);
@@ -188,29 +195,36 @@ router.put('/withdraw', auth, async (req, res, next) => {
 router.post('/transfer', auth, async (req, res, next) => {
   try {
     // Destructure request body
-    const { receiver_username, amount } = req.body;
+    const { payload } = req.body;
     const sender_username = req.username
 
-     const payload = jwt.verify(amount, process.env.SECRET);
+    const load = jwt.verify(payload, process.env.SECRET);
+    
+    console.log(load, "load")
 
-    console.log(payload, 'payload');
+    const verifiedAmount = load.user.tansfer_amount;
+    const receiver_username = load.user.receiver_username
 
-     const verifiedAmount = payload.user;
+    if (verifiedAmount >= 1000000) {
+      return res.status(400).json({ data: {}, status_code: 400, success: false, message: 'Max transfer amount is 1,000,000 per day' });
+    } 
 
     // check receiver wallet exists
     const receiver_wallet = await knex('wallet').where({ wallet_id: receiver_username });
 
     // Throw error is user with entered username does not exist
     if (receiver_wallet.length === 0) {
-     return  res.json({ data: {}, status_code: 400, success: false, message: 'A user with this username does not exist' });
+     return res.status(400).json({ data: {}, status_code: 400, success: false, message: 'A user with this username does not exist' });
     }
 
+    console.log(sender_username, 'sender_username');
+    console.log(verifiedAmount, 'verifiedAmount');
     // Check wallet for sufficient balance
     const sender_wallet_balance_validation = await knex('wallet').where({ wallet_id: sender_username }).having('balance', '>', verifiedAmount);
 
     // Throw error is user does not exist
     if (sender_wallet_balance_validation.length === 0) {
-      return res.json({ data: {}, status_code: 400, success: false, message: 'Insuffucient funds' });
+      return res.status(400).json({ data: {}, status_code: 400, success: false, message: 'Insuffucient funds' });
     } else {
       const sender_old_balance = await knex('wallet').select().where({ wallet_id: sender_username });
 
@@ -219,8 +233,8 @@ router.post('/transfer', auth, async (req, res, next) => {
       const receiver_balance = receiver_wallet[0].balance || 0
 
       // Make transaction
-      const sender_new_balance = sender_balance - verifiedAmount;
-      const receiver_new_balance = receiver_balance + verifiedAmount;
+      const sender_new_balance = parseInt(sender_balance) - parseInt(verifiedAmount);
+      const receiver_new_balance = parseInt(receiver_balance) + parseInt(verifiedAmount);
 
       // Update balance
       const updated_sender_wallet = await knex('wallet').where({ wallet_id: sender_username }).update({ balance: sender_new_balance });
@@ -253,7 +267,7 @@ router.post('/transfer', auth, async (req, res, next) => {
       
       const receiver_transaction = await knex('transactions').insert({ ...receiver_transaction_data });
 
-     return  res.json({ data: debited_balance[0], status_code: 200, success: true });
+     return res.status(200).json({ data: debited_balance[0], status_code: 200, success: true, message: "Transfer successful" });
     }
    
   } catch (error) {
